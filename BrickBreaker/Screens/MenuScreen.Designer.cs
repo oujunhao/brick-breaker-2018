194f4485030ebded25c31d14f42bc37e0e79651d
Cameron Teasdale <camteasdale143@gmail.com>
2018-05-02 08:24:39 -0400

    initial commit

--- BrickBreaker/Screens/MenuScreen.Designer.cs
+++ BrickBreaker/Screens/MenuScreen.Designer.cs
@@ -28,59 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.playButton = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuScreen));
+            this.onePlayerButton = new System.Windows.Forms.Button();
+            this.twoPlayerButton = new System.Windows.Forms.Button();
+            this.brickBreakerTitleLable = new System.Windows.Forms.Label();
             this.exitButton = new System.Windows.Forms.Button();
+            this.highscoresButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // playButton
+            // onePlayerButton
             // 
-            this.playButton.BackColor = System.Drawing.Color.White;
-            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.playButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.playButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.playButton.Location = new System.Drawing.Point(310, 297);
-            this.playButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(272, 120);
-            this.playButton.TabIndex = 0;
-            this.playButton.Text = "Play";
-            this.playButton.UseVisualStyleBackColor = false;
-            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            this.onePlayerButton.BackColor = System.Drawing.Color.Transparent;
+            this.onePlayerButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.onePlayerButton.FlatAppearance.BorderSize = 0;
+            this.onePlayerButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.onePlayerButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.onePlayerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.onePlayerButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onePlayerButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(102)))));
+            this.onePlayerButton.Location = new System.Drawing.Point(291, 216);
+            this.onePlayerButton.Name = "onePlayerButton";
+            this.onePlayerButton.Size = new System.Drawing.Size(181, 51);
+            this.onePlayerButton.TabIndex = 0;
+            this.onePlayerButton.Text = "1 Player";
+            this.onePlayerButton.UseVisualStyleBackColor = false;
+            this.onePlayerButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // twoPlayerButton
+            // 
+            this.twoPlayerButton.BackColor = System.Drawing.Color.Transparent;
+            this.twoPlayerButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.twoPlayerButton.FlatAppearance.BorderSize = 0;
+            this.twoPlayerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.twoPlayerButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.twoPlayerButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(102)))));
+            this.twoPlayerButton.Location = new System.Drawing.Point(291, 273);
+            this.twoPlayerButton.Name = "twoPlayerButton";
+            this.twoPlayerButton.Size = new System.Drawing.Size(181, 46);
+            this.twoPlayerButton.TabIndex = 1;
+            this.twoPlayerButton.Text = "2 Player";
+            this.twoPlayerButton.UseVisualStyleBackColor = false;
+            this.twoPlayerButton.Click += new System.EventHandler(this.twoPlayerButton_Click);
+            // 
+            // brickBreakerTitleLable
+            // 
+            this.brickBreakerTitleLable.AutoSize = true;
+            this.brickBreakerTitleLable.BackColor = System.Drawing.Color.Transparent;
+            this.brickBreakerTitleLable.Font = new System.Drawing.Font("Rustico", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brickBreakerTitleLable.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.brickBreakerTitleLable.Location = new System.Drawing.Point(229, 127);
+            this.brickBreakerTitleLable.Name = "brickBreakerTitleLable";
+            this.brickBreakerTitleLable.Size = new System.Drawing.Size(336, 86);
+            this.brickBreakerTitleLable.TabIndex = 2;
+            this.brickBreakerTitleLable.Text = "Brick Breaker";
             // 
             // exitButton
             // 
-            this.exitButton.BackColor = System.Drawing.Color.White;
+            this.exitButton.BackColor = System.Drawing.Color.Transparent;
             this.exitButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.exitButton.FlatAppearance.BorderSize = 0;
             this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.exitButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.exitButton.Location = new System.Drawing.Point(310, 429);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(102)))));
+            this.exitButton.Location = new System.Drawing.Point(291, 379);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(272, 120);
-            this.exitButton.TabIndex = 1;
+            this.exitButton.Size = new System.Drawing.Size(181, 46);
+            this.exitButton.TabIndex = 3;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = false;
-            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click_1);
+            // 
+            // highscoresButton
+            // 
+            this.highscoresButton.BackColor = System.Drawing.Color.Transparent;
+            this.highscoresButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.highscoresButton.FlatAppearance.BorderSize = 0;
+            this.highscoresButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.highscoresButton.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highscoresButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(102)))));
+            this.highscoresButton.Location = new System.Drawing.Point(278, 327);
+            this.highscoresButton.Name = "highscoresButton";
+            this.highscoresButton.Size = new System.Drawing.Size(210, 46);
+            this.highscoresButton.TabIndex = 4;
+            this.highscoresButton.Text = "Highscores";
+            this.highscoresButton.UseVisualStyleBackColor = false;
+            this.highscoresButton.Click += new System.EventHandler(this.highscoresButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(101, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 221);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // MenuScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.highscoresButton);
             this.Controls.Add(this.exitButton);
-            this.Controls.Add(this.playButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Controls.Add(this.brickBreakerTitleLable);
+            this.Controls.Add(this.twoPlayerButton);
+            this.Controls.Add(this.onePlayerButton);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "MenuScreen";
-            this.Size = new System.Drawing.Size(1200, 846);
+            this.Size = new System.Drawing.Size(800, 550);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button onePlayerButton;
+        private System.Windows.Forms.Button twoPlayerButton;
+        private System.Windows.Forms.Label brickBreakerTitleLable;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button highscoresButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- BrickBreaker/Screens/MenuScreen.cs
+++ BrickBreaker/Screens/MenuScreen.cs
@@ -12,14 +12,23 @@ namespace BrickBreaker
 {
     public partial class MenuScreen : UserControl
     {
+        
+
+        public void drawTriangle(Point position, int sideLength) {
+            Graphics g = this.CreateGraphics();
+            Pen trianglePen = new Pen(Color.Red, 10);
+            Point[] triangle = new Point[3];
+            triangle[0] = new Point(position.X, position.Y);
+            triangle[1] = new Point(position.X + sideLength, position.Y);
+            triangle[2] = new Point(position.X + sideLength/2, position.Y - (int)((double)sideLength * Math.Sin(45)) );
+            g.DrawPolygon(trianglePen, triangle);
+        }
         public MenuScreen()
         {
             InitializeComponent();
-        }
+            
+            
 
-        private void exitButton_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
 
         private void playButton_Click(object sender, EventArgs e)
@@ -33,6 +42,22 @@ namespace BrickBreaker
 
             gs.Location = new Point((form.Width - gs.Width) / 2, (form.Height - gs.Height) / 2);
         }
+
+        private void twoPlayerButton_Click(object sender, EventArgs e)
+        {
+            drawTriangle(new Point(70, 100), 500);
+        }
+
+        private void highscoresButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
 
     }
 }
